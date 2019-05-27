56ab28d0d34a256fab5110f2c1d631eaba68f7e0
amt1 <atreshansky@googlemail.com>
2019-05-27 23:28:44 +0100

    got links working

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -4,10 +4,9 @@ import FilmTitleLink from "./FilmTitleLink";
 class FilmList extends Component {
 
   render(){
-
     const filmTitleNodes = this.props.data.map(filmTitleLink => {
       return(
-        <FilmTitleLink>{filmTitleLink.name}</FilmTitleLink>
+        <FilmTitleLink><a href={filmTitleLink.url}>{filmTitleLink.name}</a></FilmTitleLink>
       );
     })
     return(
